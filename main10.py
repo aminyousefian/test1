7ffeb40cbfabeaff9e23f25cd16ab0b615e034dd
aminyousefian <yousefianamin85@gmail.com>
2022-05-14 09:21:29 +0430

    python commit

---
+++ main10.py
@@ -0,0 +1,23 @@
+#this is python code its tenth code in second file
+# 
+
+
+print ("--------------")
+
+tuplenumber2=tuple([6,7,8,9])
+print (tuplenumber2[1])
+print (tuplenumber2[2])
+
+print ("--------------------")
+
+#locations={
+#    (35.67 , 45.87) : "tehran",
+#   (40.39 , 69.92) : "shiraz"
+#}
+#print (locations([35.67,45.87]))
+
+print ("-----------------------------")
+
+tupple1=(1,2,3,4,5)
+for num in tupple1:
+    print (num)
